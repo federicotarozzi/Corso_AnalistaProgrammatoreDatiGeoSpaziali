f80babc0719cd6adcfb360c5e408d7d8a7eabee9
Federico-Tarozzi <161706894+federicotarozzi@users.noreply.github.com>
2024-05-23 16:01:59 +0200

    prova due

--- 01_python.py
+++ 01_python.py
@@ -1,6 +1,9 @@
-print("Prosciutto")
+#print("Prosciutto")
+idk = ["non saprei", "non lo so", "non so",  "bho"]
+laura = input("cosa mangiamo stasera? ")
 
-laura = input("cosa mangiamo staserra? ")
-if laura == "bho":
-    
-else: print("allora mangeremo del gelato")
+if laura not in idk:
+    # Aggiungi qui cosa fare se la risposta è diversa da "idk"
+    print("Ok, mi sembra interessante.")
+else:
+    print("allora mangeremo del gelato")
