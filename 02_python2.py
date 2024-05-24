b89bc7a2cb8e2c4db66682ccf6d885f0b8d1feac
Federico-Tarozzi <161706894+federicotarozzi@users.noreply.github.com>
2024-05-24 10:00:58 +0200

    sd

--- 02_python2.py
+++ 02_python2.py
@@ -1,1 +1,19 @@
+# import this funzione interna a Python che contiene le fondamenta ideologiche del linguaggio.
+import this
+'''
+nome = "Federico"
+print(nome)
+
+print("Hello world!")
+
+a = 1
+b = 2
+c = a + b 
+
+print(c) 
+'''
+
+
+
+
 
