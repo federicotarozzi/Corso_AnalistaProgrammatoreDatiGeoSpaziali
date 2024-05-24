26bbb6e2590acd95b4bbe273ebb4d5d228cd64cd
Federico-Tarozzi <161706894+federicotarozzi@users.noreply.github.com>
2024-05-24 11:47:25 +0200

    ESERCITAZIONE CON IF

--- Esercitazione_1.py
+++ Esercitazione_1.py
@@ -1,1 +1,10 @@
-###
+# ESERCIZIO: scrivere un piccolo programma che verifichi se una varaibile "numero intero" sia pari o dispari
+
+numero_int = int(input("Inserisci un numero: "))
+#numero_int = 22
+prova = (numero_int % 2)
+if prova == 0:
+    print("il tuo numero è PARI!")
+else: 
+    print("il tuo numero è DISPARI!")
+
