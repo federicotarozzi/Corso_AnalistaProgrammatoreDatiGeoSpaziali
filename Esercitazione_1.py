aa09af11452efb1cd2d678e6211322d995d4628c
Federico-Tarozzi <161706894+federicotarozzi@users.noreply.github.com>
2024-05-24 12:18:12 +0200

    Update Esercitazione_1.py

--- Esercitazione_1.py
+++ Esercitazione_1.py
@@ -8,3 +8,23 @@ if prova == 0:
 else: 
     print("il tuo numero è DISPARI!")
 
+
+# CASTING --> trasformare un tipo a un altro. occhio perchè si perdono infomraizoni. 
+
+
+
+# metodo "isinstance" ritorna vero se la varaibile utente è di tipo intero. 
+# oggetto è parte della calsse che ho definito
+
+'''
+numero = 2 # se io inserissi una string, mi troverebbe un errore nel primo if e quindi mi stamperebbe "effettuo il casting perchè la variabile non è un int"
+if (not(isinstance(numero, int))):
+    print("effettuo il casting perchè la variabile non è un int")
+    numero = int(numero)
+
+if (numero % 2):
+    print("Numero disapri")
+else:
+    print("Numero Pari")
+
+'''
