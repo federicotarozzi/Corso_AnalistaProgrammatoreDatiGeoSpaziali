21c6bd9b423de478d3d2facf5b12688757d17459
Federico-Tarozzi <161706894+federicotarozzi@users.noreply.github.com>
2024-05-24 14:56:59 +0200

    commenti

--- Esercitazione_3.py
+++ Esercitazione_3.py
@@ -1,9 +1,14 @@
+# scrivere un programma per aprire una porta: chiedi di inserire una chiave e assicurati che sia corretta per dare accesso alla porta. 
+# dai accesso se il codice inserito corrisponde alla chiave
+# il pin deve essere lungo 4 caratteri 
 
-code = int(input("inserisci il codice di accesso: "))
-key = 1234
-
-if code == key:
-    print("Codice esatto, Accesso Consentito")
+code = input("inserisci il codice di accesso a 4 cifre: ")
+key = "1234"
+if len(code)== 4:
+    if code == key:
+        print("Codice esatto, Accesso Consentito")
+    else:
+        print("Accesso Negato!")
 else:
-    print("Accesso Negato!")
+    print("la chiave deve avere 4 cifre")
 
