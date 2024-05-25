3df78c5b404fec4cd155776ca50ab9eda80b8b59
Federico-Tarozzi <161706894+federicotarozzi@users.noreply.github.com>
2024-05-25 15:58:32 +0200

    hh

--- esercitazione_4.py
+++ esercitazione_4.py
@@ -7,7 +7,7 @@ key = "1234" # chiave di accesso
 #val_inizio = 1
 #val_max = 3 # parte da 0-1-2
 # and (val_inizio <= val_max) 
-counter = 0
+counter = 0 # variabile temporale che inizia da 0
 correct_key = False
 while((not correct_key)and(counter <= 2)): # metto tutto all'interno di un ciclo while 
     code = input("inserisci il codice di accesso a 4 cifre: ")
@@ -22,7 +22,7 @@ while((not correct_key)and(counter <= 2)): # metto tutto all'interno di un ciclo
             print("la chiave deve avere 4 cifre") 
     else:
         print("la chiave inserita non è composta da cifre")
-    counter += 1
+    counter += 1 # il caounter + uno lo matto alla fine del codice. 
 
 # il loop si fa partire all'inizio e si fa finire alla fine fine di ogni condizione. 
 
