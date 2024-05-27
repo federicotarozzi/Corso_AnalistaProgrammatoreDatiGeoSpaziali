d2ae2790512e2f9f62c9491cd87ed456c9d59f82
Federico-Tarozzi <161706894+federicotarozzi@users.noreply.github.com>
2024-05-27 11:14:47 +0200

    dinello

--- esercitazione_4.py
+++ esercitazione_4.py
@@ -11,17 +11,17 @@ counter = 0 # variabile temporale che inizia da 0
 correct_key = False
 while((not correct_key)and(counter <= 2)): # metto tutto all'interno di un ciclo while 
     code = input("inserisci il codice di accesso a 4 cifre: ")
-    if code[0] in cifre and code[1] in cifre and code[2] in cifre and code[3] in cifre:  # se il primo carattere in cifre e così via... allora mi conferma che tutte le 4 cifre sono cifre
-        if len(code)== 4: # verifico che la lunghezza della chiave sia 4
+    if len(code)== 4: # verifico che la lunghezza della chiave sia 4
+        if code[0] in cifre and code[1] in cifre and code[2] in cifre and code[3] in cifre:  # se il primo carattere in cifre e così via... allora mi conferma che tutte le 4 cifre sono cifre
             if code == key: # mi assicuro che il codice inseriro dall'utente sia uguale alla mia chiave 
                 print("Codice esatto, Accesso Consentito")
                 correct_key = True # se arriva a questo punto ed è corretto allora la variabile diventa true
             else:
                 print("Accesso Negato!")
         else:
-            print("la chiave deve avere 4 cifre") 
+            print("la chiave inserita non è composta da cifre")
     else:
-        print("la chiave inserita non è composta da cifre")
+        print("la chiave deve avere 4 cifre")
     counter += 1 # il caounter + uno lo matto alla fine del codice. 
 
 # il loop si fa partire all'inizio e si fa finire alla fine fine di ogni condizione. 
