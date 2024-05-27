b7ee01c8160227f68d013041ebe76b4512493900
Federico-Tarozzi <161706894+federicotarozzi@users.noreply.github.com>
2024-05-27 15:45:50 +0200

    esercizio disegno di *

--- Esercitazione_5.py
+++ Esercitazione_5.py
@@ -1,2 +1,15 @@
  
 
+# esercizio di disegno
+# disegnare una matriec NxN di caratteri *, dove N = 4
+"""
+* * * *
+* * * *
+* * * *
+* * * *
+"""
+
+for i in range(0, 4):
+    for j in range (0, 4):
+        print("*", end=" ")
+    print()
