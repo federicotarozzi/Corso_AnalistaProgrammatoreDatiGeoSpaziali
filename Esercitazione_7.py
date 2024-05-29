5ded0c1b7f835c27180576c666c232e4a5e521f9
Federico-Tarozzi <161706894+federicotarozzi@users.noreply.github.com>
2024-05-29 17:25:56 +0200

    ,jhgj

--- Esercitazione_7.py
+++ Esercitazione_7.py
@@ -1,1 +1,23 @@
 
+# ESERCIZIO #
+
+l1 = [1,2,3,4,3,3,7,8,3]
+# 3 è in posizione: 2,4,5,8
+#print(l1)
+index3 = l1.index(3)
+# print("il numero di posizione del numero 3 nella lista l1 è: ", index3)
+
+
+       
+
+for i in l1:
+   if i == 3:
+     print("il numero di posizione del numero 3 nella lista l1 è: ", index3) + i
+     
+
+      
+   
+
+   
+     
+   
