af6da2f4f855a5d02496caf89de3f3dacc221e40
Federico-Tarozzi <161706894+federicotarozzi@users.noreply.github.com>
2024-06-03 12:41:15 +0200

    esercizio liste

--- Esercitazione_8.py
+++ Esercitazione_8.py
@@ -1,1 +1,15 @@
+#Esercizio con le liste#
+# richiedo all'utente l'inserimento di una lista di numeri interi
+# stampa la lista di numeri come li ha inserito l'utente 
+# stampa la lista di numeri in ordine inverso 
+
+
+lista_user = []
+for i in range (5):
+    lista_user.append(input("Inserisci un numero che vuoi faccia parte della tua lista: "))
+
+print("ecco la tua lista", lista_user)
+lista_user.reverse() 
+print("ecco la tua lista al contrario",lista_user)
+
 
