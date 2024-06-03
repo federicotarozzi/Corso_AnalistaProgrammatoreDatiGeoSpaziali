459a752d995cfbd7e1a480f348002050c811ff9f
Federico-Tarozzi <161706894+federicotarozzi@users.noreply.github.com>
2024-06-03 13:14:22 +0200

    asda

--- Esercitazione_8.py
+++ Esercitazione_8.py
@@ -3,6 +3,8 @@
 # stampa la lista di numeri come li ha inserito l'utente 
 # stampa la lista di numeri in ordine inverso 
 
+"""
+# LA MIA VERSIONE... 
 
 lista_user = []
 for i in range (5):
@@ -12,4 +14,50 @@ print("ecco la tua lista", lista_user)
 lista_user.reverse() 
 print("ecco la tua lista al contrario",lista_user)
 
+"""
 
+
+user_inputs = []
+
+while(True):
+    try:
+        numero = input("INSERIRE UN NUMERO: ")
+        if(numero == "end"):
+            break
+        else:
+            user_inputs.append(int(numero))
+    except ValueError:
+        print("nuemro inserito non valido, ti ricordo di interire un intero")
+        continue
+
+print("STAMPO LA LISTA IN ORDINE DI INSERIMENTO: ")
+print(user_inputs)
+
+user_inputs.reverse()
+print("ORA, IN ORDINE INVERSO")
+print(user_inputs)
+
+# ORA CHIEDO ALL'UTENTE DI INEREIRE UN ALTRO UMERO E VADO A RICERCARE QUEL NUMERO ALL'INTENRO DELLA LISTA
+# DICO ALL'UTENTE LA POSIZIONE SE OCCORRE. 
+
+numerodacercare = int(input("Inserire numero da ricercare"))
+if numerodacercare in user_inputs:
+     print("numero in lista")
+else:
+     print("numero non in lista")
+
+
+
+"""
+# qui ho provato un'altra cosa con il try e except ma non ha senso perchè non si realizza mai l'except dal momento che il numero che chiedo di inseire sarà per forza nella lista !!!!
+try:
+        elementToFind = (int(input("inserisci un nuovo numero: ")))
+        user_inputs.append(elementToFind)
+        posizione = user_inputs.index(elementToFind)
+except ValueError:
+    print("il numero che hai inserito non occorre nella lista da te indicata")
+
+print("la tua nuova lista è: ", user_inputs)
+print(posizione)
+
+"""
