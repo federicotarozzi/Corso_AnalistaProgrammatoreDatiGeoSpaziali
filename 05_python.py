1c10765dcd183b57de5f95ec6903400b1faca377
Federico-Tarozzi <161706894+federicotarozzi@users.noreply.github.com>
2024-06-05 17:21:18 +0200

    def

--- 05_python.py
+++ 05_python.py
@@ -8,6 +8,7 @@ print(type(my_tuple))
 
 my_tuple = (1,2,3) # se la riinizializzo posso modificarla per intero
 print(my_tuple)
+
 my_tuple = ("Mario", "rossi", "051_6611067")
 # posso anche inserire elementi disomogenei (interi, sting, list ecc...)
 
@@ -17,7 +18,54 @@ my_tuple = ("Mario", "rossi", "051_6611067")
 for i in my_tuple:
     print(i)
 
-print(len(my_tuple)) # 3
-print(my_tuple.count("Mario"))
+print(len(my_tuple)) #  numemro di elementi all'intenro della tuple = 3
+print(my_tuple.count("Mario")) # numero di occorrenze dell'elemento
 
+print("l' elemento {} compare {} volte".format(my_tuple[0], my_tuple.count(my_tuple[0])))
+
+
+
+### FUNZIONI ###
+# riutilizzare lo stesso pezzo di codice più volte
+# richiamo una funzione che prende delle robe me le elabora e me le restituisce. 
+
+'''
+def nome_funzione(<parametri>):  def: definisco -- do un nome alla funzione -- (inseriesco tra parentesi i parametri)
+    <istruzioni>  # definisco il corpo della funzione
+    return  # dico quello che ritorna
+
+'''
+# funzione che esegue una lista di istruzioni, in questo caso non ritorna niente
+def saluta():
+    print("ciao!")
+
+saluta()
+
+
+# molto cosigliato inserrie una testo che spiega cosa fa la mia funzione 
+
+def saluta(nome):
+    # funzione saluta stampa ciao con il nome inserto dall'utente. 
+    print("Ciao", nome)
+
+#input_utente = input("inserisic un nome: ")
+#saluta(input_utente)
+
+
+def saluta(nome):
+    stringa_return = "Ciao " + nome # creo una variabile da ritornare
+    return stringa_return # ritorno la variabile 
+
+print(saluta("Mario"))
+
+
+
+def somma(a, b):
+    return a + b
+
+numero1 = 10
+numero2 = 3
+
+risultato = somma(numero1, numero2)
+print(risultato)
 
--- Esercitazione_10.py
+++ Esercitazione_10.py
@@ -1,1 +1,59 @@
 
+def somma(a, b):
+    risultato = a + b
+    return risultato
+
+def differenza(a, b):
+    risultato = a - b
+    return risultato
+
+def moltiplicazione(a, b):
+    risultato = a * b
+    return risultato
+
+def divisione(a, b):
+    if b == 0:
+        print("Dividere un numero per zero non si può fare")
+    risultato = a / b
+    return risultato
+
+
+def calcola (num1,num2,operazione):
+    if type(num_1) == int and type(num_2) == int:
+        if operazione == "somma":
+            return somma(num_1,num_2)
+        elif operazione == "differenza":
+                return differenza(num_1,num_2)
+        elif operazione == "moltiplicazione":
+                return moltiplicazione(num_1,num_2)
+        elif operazione == "divisione":
+                return divisione(num_1,num_2)
+        else:
+            print("l'operazione da te inserrita non è stata riconosciuta dal programma")
+    else:
+        print("non hai inserito un numero intero")
+
+num_1 = int(input("Forniscimi un nuemro: "))
+operazione = input("inserisci l'operatore ")
+num_2 = int(input("Forniscimi un nuemro: "))
+
+print(calcola(num_1,num_2,operazione))
+
+
+
+"""
+operazione = input("inserisci l'operazione: somma, differenza, moltiplicazione, divisione\n ")
+if type(num_1) == int and type(num_2) == int:
+    if operazione == "somma":
+        print(somma(num_1,num_2))
+    elif operazione == "differenza":
+        print(differenza(num_1,num_2))
+    elif operazione == "moltiplicazione":
+        print(moltiplicazione(num_1,num_2))
+    elif operazione == "divisione":
+        print(divisione(num_1,num_2))
+    else:
+        print("l'operazione da te inserrita non è stata riconosciuta dal programma")
+else:
+    print("non hai inserito un numero intero")
+"""
