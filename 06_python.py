7eb4e72b1c5f3121dc36dc446b3f445088eff997
Federico-Tarozzi <161706894+federicotarozzi@users.noreply.github.com>
2024-06-12 17:28:59 +0200

    kuh

--- 06_python.py
+++ 06_python.py
@@ -2,7 +2,18 @@
 # I/O 
 import os # è il modulo che contiene le funzioni relative a input e output
 
+"""
+file = open("file.txt", "r")
+
+# un file può essere aperto in fomrato: write(sovrascrivere), append(aggiungere), read (modalità di lettura)
+
+contenuto = file.read()
+print(contenuto)
+file.close
+"""
 
 
-
-
+file = open('file.txt', "r")
+for riga in file.readlines:
+    print(riga)
+file.close
