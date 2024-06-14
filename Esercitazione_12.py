30f68ec385a40149b620375976e0775d70b5bed2
Federico-Tarozzi <161706894+federicotarozzi@users.noreply.github.com>
2024-06-14 16:49:03 +0200

    cccp

--- Esercitazione_12.py
+++ Esercitazione_12.py
@@ -1,1 +1,33 @@
 
+
+# DEFINISCI UNA CLASSE:
+"""
+
+
+"""
+class Persona:
+    def __init__(self, nome, cognome, eta, indirizzo): 
+        self.nome = nome # richiamo gli elementi dell'oggetto
+        self.cognome = cognome
+        self.eta = eta 
+        self.indirizzo = indirizzo
+
+    
+    def modifica_indirizzo(self):
+          print("il nuovo indirizzo è: ")
+    
+    def aggiorna_eta(self):
+          print("la nuova eta è: ")
+
+
+
+persona1 = Persona("Federico", "Tarozzi", "23", "via san carlo 45")
+persona1.modifica_indirizzo()
+persona1.aggiorna_eta()
+print("eta nuova: ", persona1.eta)
+print("nuovo indirizzo modificato: ", persona1.indirizzo)
+
+
+
+
+       
